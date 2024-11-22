cfeb47b93c1d29c2ddcf4cf369ea16faa1070a98
BuildTools <unconfigured@null.spigotmc.org>
2024-11-22 12:40:31 +0100

    add command to set any status including with color code

--- src/main/java/de/unvii/statusNamePlugin/StatusNamePlugin.java
+++ src/main/java/de/unvii/statusNamePlugin/StatusNamePlugin.java
@@ -1,17 +1,45 @@
 package de.unvii.statusNamePlugin;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class StatusNamePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(command.getName().equalsIgnoreCase("status")) {
+            if(sender instanceof Player player) {
+                if (args.length == 1) {
+                    String status = ChatColor.translateAlternateColorCodes('&', args[0].toUpperCase());
+
+                    String chatName = String.format("[%s§r] %s", status, player.getName());
+                    String playerListName = String.format("[%s§r] %s", status, player.getName());
+
+                    player.setDisplayName(chatName);
+                    player.setPlayerListName(playerListName);
+
+                    player.sendMessage("§2Der Status '" + status + "§2' wurde gesetzt.");
+                    return true;
+                } else {
+                    // 2 arguments necessary
+                    return false;
+                }
+            } else {
+                sender.sendMessage("Only players can execute this command");
+                return true;
+            }
+        }
+        return false;
     }
 }
